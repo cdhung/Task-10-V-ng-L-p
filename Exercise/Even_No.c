87d269b8570f9b60262f2951920bd5256a9f75df
cdhung <cdhung0402@gmail.com>
2022-06-16 16:35:40 +0700

    Commit
    recommit file .c

--- Exercise/Even_No.c
+++ Exercise/Even_No.c
@@ -2,14 +2,14 @@
 
 int main(){
 	
-	int min, max;
+	int i, min, max;
 	
 	printf("Nhap min = ");
 	scanf("%d", &min);
 	printf("Nhap max = ");
 	scanf("%d", &max);
 	
-	for(int i = min; i <= max ; i++)
+	for(i = min; i <= max ; i++)
 	{
 		if(i%2==0)
 		printf("%d ", i);
